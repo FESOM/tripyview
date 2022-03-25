e731c21fd4824c63f256c51e7e7d3567be169cb1
Patrick <Patrick.Scholz@awi.de>
2022-03-25 16:54:09 +0100

    improve 3dsphere plotting

--- tripyview/sub_3dsphere.py
+++ tripyview/sub_3dsphere.py
@@ -9,7 +9,7 @@ R_earth  = 6371.0e3
 #
 #___CREATE PYVISTA OCEAN MESH___________________________________________________________
 # make potatoefication of Earth radius 
-def create_3d_ocean_mesh(mesh,potatoefac=0.5,variable='elevation'):
+def create_3d_ocean_mesh(mesh,potatoefac=0.5,variable='elevation', do_texture=False):
     print(' --> compute 3d ocean mesh')
     #_______________________________________________________________________________________
     # do topographic potatoefication of ocean mesh
@@ -58,7 +58,17 @@ def create_3d_ocean_mesh(mesh,potatoefac=0.5,variable='elevation'):
     if any(x in variable for x in ['resolution','resol']):   
         meshpv_ocean['resolution'] = mesh.n_resol/1000
     # del offset, cells, celltypes, points
-    del cells, celltypes, points
+    del cells, celltypes
+    
+    #___do texture coordinates_______________________________________________________________
+    # Initialize the texture coordinates array
+    if do_texture:
+        meshpv_ocean.active_t_coords     = np.zeros((points.shape[0], 2))
+        xs, ys, zs                       = grid_cart3d(mesh.n_x+90, mesh.n_y, R_grid/R_earth, is_deg=True)
+        meshpv_ocean.active_t_coords[:,0]= 0.5 + np.arctan2(-xs,ys)/(2 * np.pi)
+        meshpv_ocean.active_t_coords[:,1]= 0.5 + np.arcsin(zs)/np.pi
+        del xs, ys, zs
+    del points
     
     return meshpv_ocean
 
@@ -66,7 +76,8 @@ def create_3d_ocean_mesh(mesh,potatoefac=0.5,variable='elevation'):
 #
 #
 #___CREATE PYVISTA LAND MESH TO FILL HOLES______________________________________________
-def create_3d_land_mesh(mesh, resol=1, potatoefac=1, do_topo=False, topo_path=[], topo_varname='topo', topo_dimname=['lon','lat']):
+def create_3d_land_mesh(mesh, resol=1, potatoefac=1, do_topo=False, topo_path=[], 
+                        topo_varname='topo', topo_dimname=['lon','lat'], do_texture=True):
     print(' --> compute 3d land mesh')
     
     from matplotlib.path import Path
@@ -160,8 +171,19 @@ def create_3d_land_mesh(mesh, resol=1, potatoefac=1, do_topo=False, topo_path=[]
     meshpv_land  = pv.UnstructuredGrid(cells, celltypes, points)
     # meshpv_land  = pv.UnstructuredGrid(offset, cells, celltypes, points)
     #del offset, cells, celltypes, points
-    del cells, celltypes, points
+    del cells, celltypes
 
+    #___do texture coordinates_______________________________________________________________
+    # Initialize the texture coordinates array
+    if do_texture:
+        meshpv_land.active_t_coords      = np.zeros((points.shape[0], 2))
+        xs, ys, zs                       = grid_cart3d(land_points[:,0]+90, land_points[:,1], R_grid/R_earth, is_deg=True)
+        meshpv_land.active_t_coords[:,0] = 0.5 + np.arctan2(-xs,ys)/(2 * np.pi)
+        meshpv_land.active_t_coords[:,1] = 0.5 + np.arcsin(zs)/np.pi
+        del xs, ys, zs
+    del points
+    
+    #_______________________________________________________________________________________
     return meshpv_land
 
 
@@ -191,61 +213,85 @@ def create_3d_coastline(mesh):
 #
 #___CREATE PYVISTA 3D LONGITUDE GRID_______________________________________________________
 def create_3d_lonlat_grid(dlon=30,dlat=15,potatoefac=1.0,do_topo=False):
-    print(' --> compute 3d longitude grid')    
-    
     points_lonlat_grid = list()
     
+    print(' --> compute 3d longitude grid')    
     grid_lon = np.arange(-180,180,dlon)
     dum_lat  = np.arange(-85,85+1,1)
-    
     for nlonline in range(0,len(grid_lon)):
-        if do_topo:
-            xs,ys,zs = grid_cart3d(dum_lat*0+grid_lon[nlonline], dum_lat, R_earth+(6000*100*potatoefac), is_deg=True)
-        else:    
-            xs,ys,zs = grid_cart3d(dum_lat*0+grid_lon[nlonline], dum_lat, R_earth*1.005                , is_deg=True)
+        if do_topo: xs,ys,zs = grid_cart3d(dum_lat*0+grid_lon[nlonline], dum_lat, R_earth+(6000*100*potatoefac), is_deg=True)
+        else      : xs,ys,zs = grid_cart3d(dum_lat*0+grid_lon[nlonline], dum_lat, R_earth*1.005                , is_deg=True)
         
-        points = np.vstack((xs,ys,zs)).transpose()
-    
+        points     = np.vstack((xs,ys,zs)).transpose()
         aux_points = np.column_stack((points[:-1,:],points[1:,:]))
         aux_points = np.stack((points[:-1,:],points[1:,:]), axis=2)
         aux_points = np.moveaxis(aux_points,0,1)
         aux_points = aux_points.reshape(3,2*aux_points.shape[1]).transpose()
-        #aux_points = np.row_stack((aux_points,[np.nan,np.nan,np.nan]))
-
+        
         points_lonlat_grid.append(aux_points)
-        #if nlonline == 0 : points_lonlat_grid = aux_points
-        #else :             points_lonlat_grid = np.row_stack((points_lonlat_grid,aux_points))
         del aux_points, points 
 
     #
     #
     #___CREATE PYVISTA 3D LONGITUDE GRID_______________________________________________________
     print(' --> compute 3d latitude grid')  
-    
     grid_lat = np.arange(-90+dlat,90-dlat+1,dlat)
     grid_lat = np.hstack((np.array(dum_lat[0]), grid_lat, np.array(dum_lat[-1])))
     dum_lon  = np.arange(-180,180+1,1)
-    
     for nlatline in range(0,len(grid_lat)):
-        if do_topo:
-            xs,ys,zs = grid_cart3d(dum_lon, dum_lon*0+grid_lat[nlatline], R_earth+(6000*100*potatoefac), is_deg=True)
-        else:    
-            xs,ys,zs = grid_cart3d(dum_lon, dum_lon*0+grid_lat[nlatline], R_earth*1.005, is_deg=True)
-        points = np.row_stack((xs,ys,zs)).transpose()
-    
+        if do_topo: xs,ys,zs = grid_cart3d(dum_lon, dum_lon*0+grid_lat[nlatline], R_earth+(6000*100*potatoefac), is_deg=True)
+        else      : xs,ys,zs = grid_cart3d(dum_lon, dum_lon*0+grid_lat[nlatline], R_earth*1.005, is_deg=True)
+        
+        points     = np.row_stack((xs,ys,zs)).transpose()
         aux_points = np.column_stack((points[:-1,:],points[1:,:]))
         aux_points = np.stack((points[:-1,:],points[1:,:]), axis=2)
         aux_points = np.moveaxis(aux_points,0,1)
         aux_points = aux_points.reshape(3,2*aux_points.shape[1]).transpose()
-        #aux_points = np.row_stack((aux_points,[np.nan,np.nan,np.nan]))
-
+        
         points_lonlat_grid.append(aux_points)
-        #points_lonlat_grid = np.row_stack((points_lonlat_grid,aux_points))
         del aux_points, points 
 
     return points_lonlat_grid
 
+#
+#
+#___CREATE PYVISTA 3D LONGITUDE GRID_______________________________________________________
+def create_3d_0lon0lat_grid(dlon=30,dlat=15,potatoefac=1.0,do_topo=False):
+    print(' --> compute 3d equator & 0merid line')    
+    
+    points_0lon0lat = list()
+    
+    grid_lon = [0]
+    dum_lat  = np.arange(-85,85+1,1)
+    for nlonline in range(0,len(grid_lon)):
+        if do_topo: xs,ys,zs = grid_cart3d(dum_lat*0+grid_lon[nlonline], dum_lat, R_earth+(6000*100*potatoefac), is_deg=True)
+        else      : xs,ys,zs = grid_cart3d(dum_lat*0+grid_lon[nlonline], dum_lat, R_earth*1.005                , is_deg=True)
+        
+        points     = np.vstack((xs,ys,zs)).transpose()
+        aux_points = np.column_stack((points[:-1,:],points[1:,:]))
+        aux_points = np.stack((points[:-1,:],points[1:,:]), axis=2)
+        aux_points = np.moveaxis(aux_points,0,1)
+        aux_points = aux_points.reshape(3,2*aux_points.shape[1]).transpose()
+        
+        points_0lon0lat.append(aux_points)
+        del aux_points, points 
+    
+    grid_lat = [0]
+    dum_lon  = np.arange(-180,180+1,1)
+    for nlatline in range(0,len(grid_lat)):
+        if do_topo: xs,ys,zs = grid_cart3d(dum_lon, dum_lon*0+grid_lat[nlatline], R_earth+(6000*100*potatoefac), is_deg=True)
+        else      : xs,ys,zs = grid_cart3d(dum_lon, dum_lon*0+grid_lat[nlatline], R_earth*1.005, is_deg=True)
+        
+        points     = np.row_stack((xs,ys,zs)).transpose()
+        aux_points = np.column_stack((points[:-1,:],points[1:,:]))
+        aux_points = np.stack((points[:-1,:],points[1:,:]), axis=2)
+        aux_points = np.moveaxis(aux_points,0,1)
+        aux_points = aux_points.reshape(3,2*aux_points.shape[1]).transpose()
+        
+        points_0lon0lat.append(aux_points)
+        del aux_points, points 
 
+    return points_0lon0lat
 #
 #
 #___________________________________________________________________________________________________________________
