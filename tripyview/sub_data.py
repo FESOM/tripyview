4272eb450de33164ad463f49627ac1785aa9ef9b
Patrick <Patrick.Scholz@awi.de>
2024-01-25 11:00:13 +0100

    fix small bugs when kicking out variables

--- tripyview/sub_data.py
+++ tripyview/sub_data.py
@@ -1149,7 +1149,7 @@ def do_interp_e2n(data, mesh, do_ie2n):
             #aux = grid_interp_e2n(mesh,data[vname].data)
             #with np.errstate(divide='ignore',invalid='ignore'):
             aux = grid_interp_e2n(mesh,data[vname].values)
-            #print(aux.shape)
+            
             # new variable name 
             vname_new = 'n_'+vname
             
@@ -1165,8 +1165,8 @@ def do_interp_e2n(data, mesh, do_ie2n):
             data[vname_new].attrs = data[vname].attrs
             
             # delete elem variable from dataset
-            data = data.drop_vars(labels=vname)
-    
+            data = data.drop_vars(vname)
+            del(aux)
     #___________________________________________________________________________
     return(data)
 
